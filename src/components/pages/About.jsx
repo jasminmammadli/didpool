4040e99c9cdf9dc833e13c1d9563ff4e1e213392
Yasemen <yasaman.mammadli@gmail.com>
2024-03-05 14:47:31 +0400

    about final

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -3,6 +3,18 @@ import About1 from "./../../assets/img/about-1.jpg";
 import About2 from "./../../assets/img/about-2.jpg";
 import About3 from "./../../assets/img/about-3.jpg";
 import About4 from "./../../assets/img/about-4.jpg";
+import Person1 from "./../../assets/img/person1.jpg";
+import Person2 from "./../../assets/img/person2.jpg";
+import Person3 from "./../../assets/img/person3.jpg";
+import Person4 from "./../../assets/img/person4.jpg";
+import Person5 from "./../../assets/img/person5.jpg";
+import Person6 from "./../../assets/img/person6.jpg";
+import { FaInstagram } from "react-icons/fa6";
+import { MdFacebook } from "react-icons/md";
+import { FaTwitter } from "react-icons/fa";
+import { BiSolidQuoteSingleLeft } from "react-icons/bi";
+import { IoMdCheckmark } from "react-icons/io";
+
 function About() {
   return (
     <div>
@@ -125,20 +137,256 @@ function About() {
       <div className="container mx-auto">
         <div className="grid grid-cols-4 gap-12 min-h-[70vh] pb-10">
           <div className="w-[265px] h-[265px] -rotate-12">
-            <img src={About1} alt="" className="w-full h-full object-cover rounded-xl" />
+            <img
+              src={About1}
+              alt=""
+              className="w-full h-full object-cover rounded-xl"
+            />
           </div>
           <div className="self-end w-[265px] h-[315px] rotate-12">
-            <img src={About2} alt="" className="w-full h-full object-cover rounded-xl" />
+            <img
+              src={About2}
+              alt=""
+              className="w-full h-full object-cover rounded-xl"
+            />
           </div>
           <div className="w-[265px] h-[265px] -rotate-12">
-            <img src={About3} alt="" className="w-full h-full object-cover rounded-xl" />
+            <img
+              src={About3}
+              alt=""
+              className="w-full h-full object-cover rounded-xl"
+            />
           </div>
           <div className="self-end w-[265px] h-[275px] rotate-12">
-            <img src={About4} alt="" className="w-full h-full object-cover rounded-xl" />
+            <img
+              src={About4}
+              alt=""
+              className="w-full h-full object-cover rounded-xl"
+            />
           </div>
         </div>
       </div>
-      <div className="about-section-reverse py-[50px] rotate-180"></div>
+      <div className="about-section-reverse py-[80px] rotate-180"></div>
+      <div className="bg-light-grey pb-20">
+        <h2 className="text-4xl font-[Inter-700] text-center text-dark-blue mb-16">
+          A team of exceptional people
+        </h2>
+        <div className="grid grid-cols-3 w-8/12 mx-auto gap-y-10 mb-14">
+          <div className="flex flex-col items-center">
+            <div className="mb-[23px]">
+              <img
+                src={Person1}
+                alt="person1"
+                className="w-[200px] h-[200px] object-cover rounded-full"
+              />
+            </div>
+            <div className="text-center">
+              <h4 className="font-[Inter-700] text-2xl mb-2">Sophia Johnson</h4>
+              <p className="text-lg font-[Inter-400] text-majorelle mb-4">
+                Chief Innovator
+              </p>
+              <div className="flex justify-center gap-3">
+                <span className="p-3 border-2 border-[#d6d4de] rounded-full">
+                  <MdFacebook />
+                </span>
+                <span className="p-3 border-2 border-[#d6d4de] rounded-full">
+                  <FaInstagram />
+                </span>
+                <span className="p-3 border-2 border-[#d6d4de] rounded-full">
+                  <FaTwitter />
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col items-center">
+            <div className="mb-[23px]">
+              <img
+                src={Person2}
+                alt="person1"
+                className="w-[200px] h-[200px] object-cover rounded-full"
+              />
+            </div>
+            <div className="text-center">
+              <h4 className="font-[Inter-700] text-2xl mb-2">Ethan Mitchell</h4>
+              <p className="text-lg font-[Inter-400] text-majorelle mb-4">
+                Head of HR
+              </p>
+              <div className="flex justify-center gap-3">
+                <span className="p-3 border-2 border-[#d6d4de] rounded-full">
+                  <MdFacebook />
+                </span>
+                <span className="p-3 border-2 border-[#d6d4de] rounded-full">
+                  <FaInstagram />
+                </span>
+                <span className="p-3 border-2 border-[#d6d4de] rounded-full">
+                  <FaTwitter />
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col items-center">
+            <div className="mb-[23px]">
+              <img
+                src={Person3}
+                alt="person1"
+                className="w-[200px] h-[200px] object-cover rounded-full"
+              />
+            </div>
+            <div className="text-center">
+              <h4 className="font-[Inter-700] text-2xl mb-2">Emma Lee</h4>
+              <p className="text-lg font-[Inter-400] text-majorelle mb-4">
+                Chief Innovation Officer
+              </p>
+              <div className="flex justify-center gap-3">
+                <span className="p-3 border-2 border-[#d6d4de] rounded-full">
+                  <MdFacebook />
+                </span>
+                <span className="p-3 border-2 border-[#d6d4de] rounded-full">
+                  <FaInstagram />
+                </span>
+                <span className="p-3 border-2 border-[#d6d4de] rounded-full">
+                  <FaTwitter />
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col items-center">
+            <div className="mb-[23px]">
+              <img
+                src={Person4}
+                alt="person1"
+                className="w-[200px] h-[200px] object-cover rounded-full"
+              />
+            </div>
+            <div className="text-center">
+              <h4 className="font-[Inter-700] text-2xl mb-2">Lucas Anderson</h4>
+              <p className="text-lg font-[Inter-400] text-majorelle mb-4">
+                UX Writing Lead
+              </p>
+              <div className="flex justify-center gap-3">
+                <span className="p-3 border-2 border-[#d6d4de] rounded-full">
+                  <MdFacebook />
+                </span>
+                <span className="p-3 border-2 border-[#d6d4de] rounded-full">
+                  <FaInstagram />
+                </span>
+                <span className="p-3 border-2 border-[#d6d4de] rounded-full">
+                  <FaTwitter />
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col items-center">
+            <div className="mb-[23px]">
+              <img
+                src={Person6}
+                alt="person1"
+                className="w-[200px] h-[200px] object-cover rounded-full"
+              />
+            </div>
+            <div className="text-center">
+              <h4 className="font-[Inter-700] text-2xl mb-2">Oliver Patel</h4>
+              <p className="text-lg font-[Inter-400] text-majorelle mb-4">
+                Chief of Cutlery
+              </p>
+              <div className="flex justify-center gap-3">
+                <span className="p-3 border-2 border-[#d6d4de] rounded-full">
+                  <MdFacebook />
+                </span>
+                <span className="p-3 border-2 border-[#d6d4de] rounded-full">
+                  <FaInstagram />
+                </span>
+                <span className="p-3 border-2 border-[#d6d4de] rounded-full">
+                  <FaTwitter />
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col items-center">
+            <div className="mb-[23px]">
+              <img
+                src={Person5}
+                alt="person1"
+                className="w-[200px] h-[200px] object-cover rounded-full"
+              />
+            </div>
+            <div className="text-center">
+              <h4 className="font-[Inter-700] text-2xl mb-2">Your Name</h4>
+              <p className="text-lg font-[Inter-400] text-majorelle mb-4">
+                Your Opportunity
+              </p>
+              <div className="flex justify-center gap-3">
+                <span className="p-3 border-2 border-[#d6d4de] rounded-full">
+                  <MdFacebook />
+                </span>
+                <span className="p-3 border-2 border-[#d6d4de] rounded-full">
+                  <FaInstagram />
+                </span>
+                <span className="p-3 border-2 border-[#d6d4de] rounded-full">
+                  <FaTwitter />
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="text-center">
+          <button className="bg-majorelle font-[Inter-400] text-lg py-4 px-9 rounded-lg text-white">
+            See open vacancies
+          </button>
+        </div>
+      </div>
+      <div className="container mx-auto py-[60px]">
+        <div className="flex justify-center mb-12">
+          <BiSolidQuoteSingleLeft className="text-5xl text-majorelle" />
+          <BiSolidQuoteSingleLeft className="text-5xl -ml-5 text-majorelle" />
+        </div>
+        <h1 className="text-5xl font-[Inter-700] text-center text-dark-blue max-w-[830px] mx-auto leading-[60px]">
+          Imagine the best product you’ve used, times it by ten, and you’ll have
+          something similar
+        </h1>
+      </div>
+      <div className="bg-majorelle py-[100px]">
+        <div className="container mx-auto">
+          <div className="flex justify-between items-center">
+            <div className="w-[60%]">
+              <h1 className="text-4xl font-[Inter-700] text-white leading-[50px] mb-10">
+                Sign up for your free trial, with the world’s best product tool
+              </h1>
+              <ul className="flex gap-10">
+                <li className="flex items-center gap-4">
+                  <span className="w-6 h-6 flex justify-center items-center bg-primary-light rounded-lg">
+                    <IoMdCheckmark className="text-xs text-majorelle" />
+                  </span>
+                  <span className="fonr-[Inter-400] text-lg text-white">No obligation</span>
+                </li>
+                <li className="flex items-center gap-4">
+                  <span className="w-6 h-6 flex justify-center items-center bg-primary-light rounded-lg">
+                    <IoMdCheckmark className="text-xs text-majorelle" />
+                  </span>
+                  <span className="fonr-[Inter-400] text-lg text-white">Free onboarding </span>
+                </li>
+                <li className="flex items-center gap-4">
+                  <span className="w-6 h-6 flex justify-center items-center bg-primary-light rounded-lg">
+                    <IoMdCheckmark className="text-xs text-majorelle" />
+                  </span>
+                  <span className="fonr-[Inter-400] text-lg text-white">14 day free trial</span>
+                </li>
+                <li className="flex items-center gap-4">
+                  <span className="w-6 h-6 flex justify-center items-center bg-primary-light rounded-lg">
+                    <IoMdCheckmark className="text-xs text-majorelle" />
+                  </span>
+                  <span className="fonr-[Inter-400] text-lg text-white">Full features</span>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <button className="bg-blue py-4 px-9 font-[Inter-400] text-lg text-white rounded-lg">
+                Start your trial
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
